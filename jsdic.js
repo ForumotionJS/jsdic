692b3bddaaf0a01902a10d4d9f5bfb234d3a08d4
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-03 15:22:03 +0800

    Update jsdic.js

--- jsdic.js
+++ jsdic.js
@@ -1,4 +1,12 @@
-getSel = window.getSelection();
+var getSel = function() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
 
 var dict = [
     { title: "window.getSelection", desc: "A range of the text that are selected" },
