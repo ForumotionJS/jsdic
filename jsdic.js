028f187e8e9dcd407037e1145709297d87e5c4a2
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-07 16:21:00 +0800

    Update jsdic.js

--- jsdic.js
+++ jsdic.js
@@ -1,32 +1,25 @@
-function getSel() {
-    var text = "";
+window.onmouseup = getSel;
+window.onkeyup = getSel;
+
+function getSel(){
+    var dict = [
+        { title: "window.getSelection", desc: "A range of the text that are selected" },
+        { title: "var", desc: "Variable of JS" },
+        { title: "document.getElementById", desc: "Get the ID of the current element" },
+        { title: "my_setcookie", desc: "Set a cookie" },
+        { title: "my_getcookie", desc: "Get a cookie" },
+        { title: "_userdata", desc: "User storage" },
+        { title: "document.createElement", desc: "Create a HTML specified element" }
+    ],
+    text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
-    return text;
-}
-
-var dict = [
-    { title: "window.getSelection", desc: "A range of the text that are selected" },
-    { titlet: "var", desct: "Variable of JS" },
-    { titlef: "document.getElementById", descf: "Get the ID of the current element" },
-    { titlew: "my_setcookie", descw: "Set a cookie" },
-    { titles: "my_getcookie", descs: "Get a cookie" },
-    { titleo: "_userdata", desco: "User storage" }
-];
-
-for (var i = 0; i < dict.length; i += 1) {
-  if(getSel() == dict[i].title) {
-    alert(dict[i].desc);
-  }else if(getSel() == dict[i].titlet) {
-    alert(dict[i].desct);
-  }else if(getSel() == dict[i].titlef) {
-    alert(dict[i].descf);
-  }else if(getSel() == dict[i].titles) {
-    alert(dict[i].descs);
-  }else if(getSel() == dict[i].titleo) {
-    alert(dict[i].desco);
-  }
+    dict.forEach(function(v){
+        if(text===v.title){
+            alert(v.desc);
+        }
+    });
 }
